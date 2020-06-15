7a84f44a2a1e985222a8b33186ae1397ba75991e
Jhonathan Tabares <tantabares@gmail.com>
2020-06-15 14:13:35 -0500

    Chapter 7 Done - (deprecated tutorial)

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ModelManager, Constants } from "@adobe/cq-spa-page-model-manager";
+import { AEMResponsiveGridComponent, AEMContainerComponent, MapTo } from "@adobe/cq-angular-editable-components";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-app';
+  constructor () {
+    ModelManager.initialize();
+  }
 }
+
+MapTo('wknd-events/component/structure/app')(AEMContainerComponent);
+MapTo('wcm/foundation/components/responsivegrid')(AEMResponsiveGridComponent);
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { SpaAngularEditableComponentsModule } from "@adobe/cq-angular-editable-components";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PageComponent } from "./components/page/page.component";
+import { TextComponent } from './components/text/text.component';
+import { ImageComponent } from './components/image/image.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PageComponent, TextComponent, ImageComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SpaAngularEditableComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [TextComponent, ImageComponent]
 })
-export class AppModule { }
+export class AppModule {}
